add5a55042f9505a4e362191f88a6cd82e8b05f8
abg007889 <abg007889@yahoo.com.hk>
2022-06-19 14:56:25 +0800

    Drag and Drop
    Add Drag and Drop function for task 1-2.
    Still need update task 3-5

--- jsFunction.js
+++ jsFunction.js
@@ -5,3 +5,18 @@ function editMemo() {
     document.getElementById("memo").innerHTML = memoText;
   }
 }
+
+//Darg and Drop function: Chloe
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
